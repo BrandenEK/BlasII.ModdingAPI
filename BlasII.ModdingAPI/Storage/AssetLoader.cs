a83a5d678503efe1348f4a1efd3afe4a0484b46e
BrandenEK <brandenek03912@hotmail.com>
2023-09-05 12:12:07 -0400

    Move assetloader to storage namespace

--- BlasII.ModdingAPI/Storage/AssetLoader.cs
+++ BlasII.ModdingAPI/Storage/AssetLoader.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-namespace BlasII.ModdingAPI
+namespace BlasII.ModdingAPI.Storage
 {
     internal static class AssetLoader
     {
@@ -21,6 +22,18 @@ namespace BlasII.ModdingAPI
             {
                 if (lookup.TryGetValue(obj.name, out string id))
                     storage.Add(id, obj);
+            }
+        }
+
+        public static void LoadObjectsOfType<T>(Dictionary<string, T> storage, string prefix) where T : ScriptableObject
+        {
+            storage.Clear();
+            var objects = Resources.FindObjectsOfTypeAll<T>().OrderBy(x => x.name);
+            int idx = 0;
+            foreach (T obj in objects)
+            {
+                storage.Add(prefix + idx.ToString("00"), obj);
+                idx++;
             }
         }
     }
