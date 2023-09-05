6f5dde6578c86d1658ed7aa68a9d69dd53a5ea47
Branden Wood <brandenek03912@hotmail.com>
2023-09-05 16:43:22 -0400

    Store more fonts and parents

--- BlasII.ModdingAPI/ModdingAPI.cs
+++ BlasII.ModdingAPI/ModdingAPI.cs
@@ -3,13 +3,14 @@ using BlasII.ModdingAPI.UI;
 using Il2CppTMPro;
 using System.Text;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace BlasII.ModdingAPI
 {
     internal class ModdingAPI : BlasIIMod
     {
         public ModdingAPI() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+        private bool _initializedUI = false;
 
         protected internal override void OnInitialize()
         {
@@ -28,7 +29,13 @@ namespace BlasII.ModdingAPI
         {
             if (sceneName == "MainMenu")
             {
-                StoreDefaultUI();
+                if (!_initializedUI)
+                {
+                    UIModder.Fonts.Initialize();
+                    UIModder.Parents.Initialize();
+                    _initializedUI = true;
+                }
+
                 DisplayModListOnMenu();
             }
         }
@@ -36,12 +43,6 @@ namespace BlasII.ModdingAPI
         protected internal override void OnSceneUnloaded(string sceneName)
         {
             
-        }
-
-        private void StoreDefaultUI()
-        {
-            UIModder.DefaultParent = Object.FindObjectOfType<CanvasScaler>()?.GetComponent<RectTransform>();
-            UIModder.DefaultFont = Object.FindObjectOfType<TextMeshProUGUI>()?.font;
         }
 
         private void DisplayModListOnMenu()
@@ -54,7 +55,7 @@ namespace BlasII.ModdingAPI
             }
 
             // Create underneath text object
-            UIModder.CreateRect("Mods under", UIModder.MainMenuParent.GetChild(0))
+            UIModder.CreateRect("Mods under", UIModder.Parents.MainMenu.GetChild(0))
                 .SetXRange(0, 0)
                 .SetYRange(1, 1)
                 .SetPivot(0, 1)
@@ -67,7 +68,7 @@ namespace BlasII.ModdingAPI
                 .SetColor(new Color(0.004f, 0.008f, 0.008f));
 
             // Create overhead text object
-            UIModder.CreateRect("Mods over", UIModder.MainMenuParent.GetChild(0))
+            UIModder.CreateRect("Mods over", UIModder.Parents.MainMenu.GetChild(0))
                 .SetXRange(0, 0)
                 .SetYRange(1, 1)
                 .SetPivot(0, 1)
