1ac553677d90f0a64f741bdb8907c2024e3cf40e
Branden Wood <brandenek03912@hotmail.com>
2023-09-22 15:25:11 -0400

    Add enum for abilities

--- BlasII.ModdingAPI/Storage/AbilityStorage.cs
+++ BlasII.ModdingAPI/Storage/AbilityStorage.cs
@@ -17,4 +17,67 @@ namespace BlasII.ModdingAPI.Storage
 
         public static IEnumerable<KeyValuePair<string, IAbilityTypeRef>> GetAllAbilities() => _abilities.OrderBy(x => x.Key);
     }
+
+    public enum AbilityType
+    {
+        // Movement
+        HorizontalMovement = 10,
+        HorizontalMovementOnAir = 11,
+        // Facing
+        FacingDirection = 20,
+        CharacterFacingDirection = 21,
+        CharacterFacingDirectionOnAir = 22,
+        // Throwing
+        ThrowObject = 30,
+        ThrowObjectDirection = 31,
+        ThrowObjectToTarget = 32,
+        // Climbing
+        LadderClimb = 40,
+        LedgeClimb = 41,
+        LedgeWallClimb = 42,
+        // Recovering
+        GroundRecovery = 50,
+        AirRecovery = 51,
+        // Actions
+        Attack = 60,
+        Crouch = 61,
+        Dash = 62,
+        Flask = 63,
+        JumpDown = 64,
+        Jump = 65,
+        InteractableUse = 66,
+        EtherealLeversUse = 67,
+        Execution = 68,
+        ChangeWeapon = 69,
+        AirDash = 70,
+        AirJump = 71,
+        // Traits
+        Instadeath = 80,
+        Stagger = 81,
+        SuperArmor = 82,
+        HardLanding = 83,
+        WallClimb = 84,
+        WallJump = 85,
+        MagicRingClimb = 86,
+        // Censer
+        CenserChargedAttack = 100,
+        CenserIgniter = 101,
+        // Blade
+        BladeBerserkModeActivator = 110,
+        BladeParry = 111,
+        VerticalImpulse = 112,
+        // Rapier
+        RapierParry = 120,
+        RapierTeleport = 121,
+        HorizontalImpulse = 122,
+        ZipLineSlide = 123,
+        // Prayers
+        FastPrayer = 130,
+        FullPrayer = 131,
+        // Unknown
+        Drag = 200,
+        GhostClimb = 201,
+        Gravity = 202,
+        ApplyDirectionalImpulse = 203,
+    }
 }
