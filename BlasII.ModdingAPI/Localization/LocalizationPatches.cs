732255a4d26e0166675372d7582cb9fdbce44720
Branden Wood <brandenek03912@hotmail.com>
2023-10-15 20:21:47 -0400

    localize: add patch when lang changes

---
+++ BlasII.ModdingAPI/Localization/LocalizationPatches.cs
@@ -0,0 +1,16 @@
+﻿using BlasII.ModdingAPI.Storage;
+using HarmonyLib;
+using Il2CppI2.Loc;
+
+namespace BlasII.ModdingAPI.Localization
+{
+    [HarmonyPatch(typeof(LocalizationManager), nameof(LocalizationManager.SetLanguageAndCode))]
+    class Language_Change_Patch
+    {
+        public static void Postfix(string LanguageCode)
+        {
+            Main.ModdingAPI.Log($"Changing language to [{LanguageCode}]");
+            ItemStorage.LocalizeAllItems();
+        }
+    }
+}
