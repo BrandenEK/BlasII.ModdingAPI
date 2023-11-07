858b2879108493420ac0e8fea0a0c3e0b9301731
Branden Wood <brandenek03912@hotmail.com>
2023-11-07 15:38:12 -0500

    docs: document audio module

--- BlasII.ModdingAPI/Audio/AudioHandler.cs
+++ BlasII.ModdingAPI/Audio/AudioHandler.cs
@@ -2,13 +2,22 @@
 
 namespace BlasII.ModdingAPI.Audio
 {
+    /// <summary>
+    /// Provides access to playing different sound effects
+    /// </summary>
     public class AudioHandler
     {
+        /// <summary>
+        /// Plays a sound effect from the UI module
+        /// </summary>
         public void PlayEffectUI(UISFX effect)
         {
             PlayEffect("event:/SFX/UI/" + _uiEvents[(int)effect]);
         }
 
+        /// <summary>
+        /// Plays a sound effect from the given ID
+        /// </summary>
         public void PlayEffect(string effect)
         {
             CoreCache.AudioManager.InstantiateEvent(effect);
