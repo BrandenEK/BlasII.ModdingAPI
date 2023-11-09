fe81470e954d3a0527017057e63f8afec4305ee1
Branden Wood <brandenek03912@hotmail.com>
2023-11-09 16:42:16 -0500

    docs: document ui module

--- BlasII.ModdingAPI/UI/RectExtensions.cs
+++ BlasII.ModdingAPI/UI/RectExtensions.cs
@@ -5,18 +5,30 @@ using UnityEngine.UI;
 
 namespace BlasII.ModdingAPI.UI
 {
+    /// <summary>
+    /// Useful methods to modify RectTransforms
+    /// </summary>
     public static class RectExtensions
     {
+        /// <summary>
+        /// Adds an Image component to the UI
+        /// </summary>
         public static Image AddImage(this RectTransform rect)
         {
             return rect.gameObject.AddComponent<Image>().ResetToDefault();
         }
 
+        /// <summary>
+        /// Adds a TextMeshProUGUI component to the UI
+        /// </summary>
         public static TextMeshProUGUI AddText(this RectTransform rect)
         {
             return rect.gameObject.AddComponent<TextMeshProUGUI>().ResetToDefault();
         }
 
+        /// <summary>
+        /// Resets xrange, yrange, pivot, position, and size to default
+        /// </summary>
         public static RectTransform ResetToDefault(this RectTransform rect)
         {
             return rect
@@ -27,6 +39,9 @@ namespace BlasII.ModdingAPI.UI
                 .SetSize(100, 100);
         }
 
+        /// <summary>
+        /// Displays the properties of the UI
+        /// </summary>
         public static string DisplayProperties(this RectTransform rect)
         {
             var sb = new StringBuilder("\n\n");
@@ -38,7 +53,7 @@ namespace BlasII.ModdingAPI.UI
             return sb.ToString();
         }
 
-
+        /// <summary> Updates the x anchors </summary>
         public static RectTransform SetXRange(this RectTransform rect, Vector2 range)
         {
             rect.anchorMin = new Vector2(range.x, rect.anchorMin.y);
@@ -46,9 +61,10 @@ namespace BlasII.ModdingAPI.UI
             return rect;
         }
 
+        /// <summary> Updates the x anchors </summary>
         public static RectTransform SetXRange(this RectTransform rect, float xMin, float xMax) => rect.SetXRange(new Vector2(xMin, xMax));
 
-
+        /// <summary> Updates the y anchors </summary>
         public static RectTransform SetYRange(this RectTransform rect, Vector2 range)
         {
             rect.anchorMin = new Vector2(rect.anchorMin.x, range.x);
@@ -56,33 +72,37 @@ namespace BlasII.ModdingAPI.UI
             return rect;
         }
 
+        /// <summary> Updates the y anchors </summary>
         public static RectTransform SetYRange(this RectTransform rect, float yMin, float yMax) => rect.SetYRange(new Vector2(yMin, yMax));
 
-
+        /// <summary> Updates the pivot </summary>
         public static RectTransform SetPivot(this RectTransform rect, Vector2 pivot)
         {
             rect.pivot = pivot;
             return rect;
         }
 
+        /// <summary> Updates the pivot </summary>
         public static RectTransform SetPivot(this RectTransform rect, float xPivot, float yPivot) => rect.SetPivot(new Vector2(xPivot, yPivot));
 
-
+        /// <summary> Updates the x and y position </summary>
         public static RectTransform SetPosition(this RectTransform rect, Vector2 position)
         {
             rect.anchoredPosition = position;
             return rect;
         }
 
+        /// <summary> Updates the x and y position </summary>
         public static RectTransform SetPosition(this RectTransform rect, float xPosition, float yPosition) => rect.SetPosition(new Vector2(xPosition, yPosition));
 
-
+        /// <summary> Updates the width and height </summary>
         public static RectTransform SetSize(this RectTransform rect, Vector2 size)
         {
             rect.sizeDelta = size;
             return rect;
         }
 
+        /// <summary> Updates the width and height </summary>
         public static RectTransform SetSize(this RectTransform rect, float xSize, float ySize) => rect.SetSize(new Vector2(xSize, ySize));
     }
 }
--- BlasII.ModdingAPI/UI/TextExtensions.cs
+++ BlasII.ModdingAPI/UI/TextExtensions.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 namespace BlasII.ModdingAPI.UI
 {
+    /// <summary>
+    /// Useful methods to modify TextMeshProUGUIs
+    /// </summary>
     public static class TextExtensions
     {
+        /// <summary>
+        /// Resets contents, color, size, alignment, wrapping, and font to default
+        /// </summary>
         public static TextMeshProUGUI ResetToDefault(this TextMeshProUGUI text)
         {
             return text
@@ -17,6 +23,9 @@ namespace BlasII.ModdingAPI.UI
                 .SetFont(UIModder.Fonts.Default);
         }
 
+        /// <summary>
+        /// Displays the properties of the UI
+        /// </summary>
         public static string DisplayProperties(this TextMeshProUGUI text)
         {
             var sb = new StringBuilder("\n\n");
@@ -29,30 +38,35 @@ namespace BlasII.ModdingAPI.UI
             return sb.ToString();
         }
 
+        /// <summary> Updates the contents </summary>
         public static TextMeshProUGUI SetContents(this TextMeshProUGUI text, string contents)
         {
             text.text = contents;
             return text;
         }
 
+        /// <summary> Updates the color </summary>
         public static TextMeshProUGUI SetColor(this TextMeshProUGUI text, Color color)
         {
             text.color = color;
             return text;
         }
 
+        /// <summary> Updates the font size </summary>
         public static TextMeshProUGUI SetFontSize(this TextMeshProUGUI text, float size)
         {
             text.fontSize = size;
             return text;
         }
 
+        /// <summary> Updates the alignment mode </summary>
         public static TextMeshProUGUI SetAlignment(this TextMeshProUGUI text, TextAlignmentOptions alignment)
         {
             text.alignment = alignment;
             return text;
         }
 
+        /// <summary> Failed to update the wrapping mode </summary>
         [System.Obsolete("This did the wrong thing.  Use SetWrapping instead")]
         public static TextMeshProUGUI SetOverflow(this TextMeshProUGUI text, TextOverflowModes overflow)
         {
@@ -60,12 +74,14 @@ namespace BlasII.ModdingAPI.UI
             return text;
         }
 
+        /// <summary> Updates the wrapping mode </summary>
         public static TextMeshProUGUI SetWrapping(this TextMeshProUGUI text, bool wordWrap)
         {
             text.enableWordWrapping = wordWrap;
             return text;
         }
 
+        /// <summary> Updates the font asset </summary>
         public static TextMeshProUGUI SetFont(this TextMeshProUGUI text, TMP_FontAsset font)
         {
             text.font = font;
