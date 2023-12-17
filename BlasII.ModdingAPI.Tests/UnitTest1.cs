7e08387fb8449eb41f9cc41a1d21f5222e0a04a2
Branden Wood <brandenek03912@hotmail.com>
2023-12-17 09:35:29 -0500

    tests: add test project

---
+++ BlasII.ModdingAPI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace BlasII.ModdingAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
