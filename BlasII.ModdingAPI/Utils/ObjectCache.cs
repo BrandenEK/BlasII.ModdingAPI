43bd4e24bd11d4089c548a2bdd8aba5fa942574d
Branden Wood <brandenek03912@hotmail.com>
2023-12-18 10:41:13 -0500

    util: add object cache

---
+++ BlasII.ModdingAPI/Utils/ObjectCache.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BlasII.ModdingAPI.Utils
+{
+    /// <summary>
+    /// A wrapper for a cacheable object that needs to be found first
+    /// </summary>
+    public class ObjectCache<T> where T : class
+    {
+        private readonly Func<T> _search;
+        private T _value = null;
+
+        /// <summary>
+        /// Initializes the search method for caching the object
+        /// </summary>
+        public ObjectCache(Func<T> search) => _search = search;
+
+        /// <summary>
+        /// Retrieves the object from the cache or by using the search method
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                if (_value == null)
+                    _value = _search();
+
+                return _value;
+            }
+        }
+    }
+}
