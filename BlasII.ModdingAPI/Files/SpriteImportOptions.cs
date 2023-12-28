a693e69e4eee0726d6c21fdfe0117b4158895192
Branden Wood <brandenek03912@hotmail.com>
2023-12-28 11:09:22 -0500

    file: document import options

--- BlasII.ModdingAPI/Files/SpriteImportOptions.cs
+++ BlasII.ModdingAPI/Files/SpriteImportOptions.cs
@@ -2,12 +2,24 @@
 
 namespace BlasII.ModdingAPI.Files
 {
+    /// <summary>
+    /// Settings used when importing a sprite
+    /// </summary>
     public class SpriteImportOptions
     {
+        /// <summary> Default: 32 </summary>
         public int PixelsPerUnit { get; init; } = 32;
+
+        /// <summary> Default: true </summary>
         public bool UsePointFilter { get; init; } = true;
+
+        /// <summary> Default: (0.5, 0.5) </summary>
         public Vector2 Pivot { get; init; } = new Vector2(0.5f, 0.5f);
+
+        /// <summary> Default: (0, 0, 0, 0) </summary>
         public Vector4 Border { get; init; } = Vector4.zero;
+
+        /// <summary> Default: Tight </summary>
         public SpriteMeshType MeshType { get; init; } = SpriteMeshType.Tight;
     }
 }
