74ff3896a04d7bd6f449464de6c7ac98a2503244
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-21 13:52:42 -0400

    utils: remove gameobject cache

--- BlasII.ModdingAPI/Utils/ObjectCache.cs
+++ BlasII.ModdingAPI/Utils/ObjectCache.cs
@@ -25,26 +25,3 @@ public class ObjectCache<T>(Func<T> search) where T : MonoBehaviour
         }
     }
 }
-
-/// <summary>
-/// A wrapper for a cacheable object that needs to be found first
-/// </summary>
-public class ObjectCache(Func<GameObject> search)
-{
-    private readonly Func<GameObject> _search = search;
-    private GameObject _value = null;
-
-    /// <summary>
-    /// Retrieves the object from the cache or by using the search method
-    /// </summary>
-    public GameObject Value
-    {
-        get
-        {
-            if (_value == null)
-                _value = _search();
-
-            return _value;
-        }
-    }
-}
