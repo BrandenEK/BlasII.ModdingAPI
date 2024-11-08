68bf76bcdc449e82065da53f927758bea1ca3782
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-08 09:12:31 -0500

    assets: experiment with DoubleIdAssets

--- BlasII.ModdingAPI/Assets/GenericSingleStorage.cs
+++ BlasII.ModdingAPI/Assets/GenericSingleStorage.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BlasII.ModdingAPI.Assets;
@@ -42,5 +44,64 @@ public class GenericSingleStorage<T> : IEnumerable<KeyValuePair<string, T>> wher
     }
 }
 
+public class SingleIdAsset<T> where T : ScriptableObject
+{
+    public string Id { get; }
+    public T Value { get; }
+}
+
+public class DoubleIdAsset<T, E> where T : ScriptableObject where E : Enum
+{
+    public string Id { get; }
+    public E StaticId { get; }
+    public T Value { get; }
+
+    public DoubleIdAsset(string id, E staticId, T value)
+    {
+        Id = id;
+        StaticId = staticId;
+        Value = value;
+    }
+}
+
 public delegate Dictionary<string, T> AssetFinder<T>();
 public delegate IEnumerable<KeyValuePair<string, T>> AssetOrderer<T>(Dictionary<string, T> assets);
+
+
+/// <summary>
+/// Stores ScriptableObject assets of a generic type
+/// </summary>
+public class GenericDoubleStorage<T, E> : IEnumerable<DoubleIdAsset<T, E>> where T : ScriptableObject where E : Enum
+{
+    private readonly IEnumerable<DoubleIdAsset<T, E>> _assets;
+
+    internal GenericDoubleStorage(IEnumerable<DoubleIdAsset<T, E>> assets)
+    {
+        _assets = assets;
+    }
+
+    /// <summary>
+    /// Retrieves an asset with the specified id
+    /// </summary>
+    public T this[string id] => _assets.FirstOrDefault(x => x.Id == id)?.Value;
+
+    /// <summary>
+    /// Retrieves an asset with the specified id
+    /// </summary>
+    public T this[E id] => _assets.FirstOrDefault(x => x.StaticId.Equals(id))?.Value;
+
+    /// <summary>
+    /// Attempts to retrieve an asset with the specified id
+    /// </summary>
+    //public bool TryGetAsset(string id, out T asset) => _assets.TryGetValue(id, out asset);
+
+    IEnumerator<DoubleIdAsset<T, E>> IEnumerable<DoubleIdAsset<T, E>>.GetEnumerator()
+    {
+        return _assets.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _assets.GetEnumerator();
+    }
+}
