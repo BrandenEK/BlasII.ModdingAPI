2648ff886be071676edd9688f3057baa27fcd6d9
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-27 10:04:59 -0500

    test: add better save/load/delete methods

--- BlasII.ModdingAPI/Persistence/PersistencePatches.cs
+++ BlasII.ModdingAPI/Persistence/PersistencePatches.cs
@@ -6,37 +6,66 @@ using System.Reflection;
 
 namespace BlasII.ModdingAPI.Persistence;
 
-[HarmonyPatch]
-class Mod_Save1_Patch
-{
-    public static MethodInfo TargetMethod()
-    {
-        return AccessTools.Method(typeof(GuiltManager), nameof(GuiltManager.BuildCurrentPersistentState), System.Array.Empty<System.Type>());
-    }
+//[HarmonyPatch]
+//class Mod_Save1_Patch
+//{
+//    public static MethodInfo TargetMethod()
+//    {
+//        return AccessTools.Method(typeof(GuiltManager), nameof(GuiltManager.BuildCurrentPersistentState), System.Array.Empty<System.Type>());
+//    }
 
-    public static void Postfix() => SaveData.SaveGame(CoreCache.SaveData.CurrentSaveSlot);
-}
-
-[HarmonyPatch(typeof(GuiltManager), nameof(GuiltManager.BuildCurrentPersistentState), typeof(PersistentData))]
+//    public static void Postfix()
+//    {
+//        ModLog.Info($"Saving game data for slot {CoreCache.SaveData.CurrentSaveSlot}");
+//        SaveData.SaveGame(CoreCache.SaveData.CurrentSaveSlot);
+//    }
+//}
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.SaveGame), [])]
 class Mod_Save2_Patch
 {
-    public static void Postfix() => SaveData.SaveGame(CoreCache.SaveData.CurrentSaveSlot);
+    public static void Prefix()
+    {
+        ModLog.Info($"Saving persistent data for slot {CoreCache.SaveData.CurrentSaveSlot}");
+        SaveData.SaveGame(CoreCache.SaveData.CurrentSaveSlot);
+    }
 }
 
-[HarmonyPatch(typeof(GuiltManager), nameof(GuiltManager.SetCurrentPersistentState))]
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.SaveGame), typeof(int))]
+class Mod_Save3_Patch
+{
+    public static void Prefix(int slot)
+    {
+        ModLog.Info($"Saving persistent data for slot {slot}");
+        SaveData.SaveGame(slot);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.LoadGame), typeof(int))]
 class Mod_Load_Patch
 {
-    public static void Postfix() => SaveData.LoadGame(CoreCache.SaveData.CurrentSaveSlot);
+    public static void Prefix(int slot)
+    {
+        ModLog.Info($"Loading persistent data for slot {slot}");
+        SaveData.LoadGame(slot);
+    }
 }
 
-[HarmonyPatch(typeof(GuiltManager), nameof(GuiltManager.ResetPersistence))]
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.ResetPersistence))]
 class Mod_Reset_Patch
 {
-    public static void Postfix() => SaveData.ResetGame();
+    public static void Prefix()
+    {
+        ModLog.Info("Resetting persistent data");
+        SaveData.ResetGame();
+    }
 }
 
 [HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.DeleteSlot))]
-class Mod_Delete_Patch
+class SaveDataManager_DeleteSlot_Patch
 {
-    public static void Postfix(int slot) => SaveData.DeleteDataFromFile(slot);
+    public static void Prefix(int slot)
+    {
+        ModLog.Info($"Deleting persistent data for slot {slot}");
+        SaveData.DeleteDataFromFile(slot);
+    }
 }
