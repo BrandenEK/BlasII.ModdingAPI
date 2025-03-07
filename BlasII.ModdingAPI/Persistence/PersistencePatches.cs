285daeba44d0808a877208e7afd32f776134d9fa
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-03-07 17:38:13 -0500

    persistence: add more patches with logs

--- BlasII.ModdingAPI/Persistence/PersistencePatches.cs
+++ BlasII.ModdingAPI/Persistence/PersistencePatches.cs
@@ -1,7 +1,9 @@
 ﻿using HarmonyLib;
 using Il2CppTGK.Game;
 using Il2CppTGK.Game.Managers;
+using Il2CppTGK.Game.Managers.SaveData;
 using Il2CppTGK.Persistence;
+using System.Drawing;
 using System.Reflection;
 
 namespace BlasII.ModdingAPI.Persistence;
@@ -39,4 +41,87 @@ class Mod_Reset_Patch
 class Mod_Delete_Patch
 {
     public static void Postfix(int slot) => SaveData.DeleteDataFromFile(slot);
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.SaveGame), [])]
+class SaveDataManager_SaveGame1_Patch
+{
+    public static void Prefix()
+    {
+        int slot = CoreCache.SaveData.CurrentSaveSlot;
+        ModLog.Custom($"Saving data for slot {slot}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.SaveGame), typeof(int))]
+class SaveDataManager_SaveGame2_Patch
+{
+    public static void Prefix(int slot)
+    {
+        ModLog.Custom($"Saving data for slot {slot}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.SaveGameInEnding))]
+class SaveDataManager_SaveGameInEnding_Patch
+{
+    public static void Prefix()
+    {
+        int slot = CoreCache.SaveData.CurrentSaveSlot;
+        ModLog.Custom($"Saving data for slot {slot}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.LoadGame))]
+class SaveDataManager_LoadGame_Patch
+{
+    public static void Prefix(int slot)
+    {
+        ModLog.Custom($"Loading data for slot {slot}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.LoadGameWithoutReset))]
+class SaveDataManager_LoadGameWithoutReset_Patch
+{
+    public static void Prefix(int slot)
+    {
+        ModLog.Custom($"Loading data for slot {slot}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.DeleteSlot))]
+class SaveDataManager_DeleteSlot_Patch
+{
+    public static void Prefix(int slot)
+    {
+        ModLog.Custom($"Deleting data for slot {slot}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.CopySlot))]
+class SaveDataManager_CopySlot_Patch
+{
+    public static void Prefix(int slotSrc, int slotDest)
+    {
+        ModLog.Custom($"Copy data for slot {slotSrc} to slot {slotDest}", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.ResetSlot))]
+class SaveDataManager_ResetSlot_Patch
+{
+    public static void Prefix()
+    {
+        ModLog.Custom($"Resetting data for all slots", Color.Blue);
+    }
+}
+
+[HarmonyPatch(typeof(SaveDataManager), nameof(SaveDataManager.ResetPersistence))]
+class SaveDataManager_ResetPersistence_Patch
+{
+    public static void Prefix()
+    {
+        ModLog.Custom($"Resetting data for all slots perseieiss", Color.Blue);
+    }
 }
