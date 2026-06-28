d3b83782b7b2a45768ed0213b913ffe11a16bc3c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-06-28 16:41:40 -0400

    test: log elemental/physical types on attack hit

--- BlasII.ModdingAPI/ModPatches.cs
+++ BlasII.ModdingAPI/ModPatches.cs
@@ -1,9 +1,61 @@
 ﻿using HarmonyLib;
 using Il2CppTGK.Framework;
+using Il2CppTGK.Game.Components.Attack;
+using Il2CppTGK.Game.Components.Attack.Data;
 using Il2CppTGK.Game.Components.UI;
 using Il2CppTGK.Game.Managers;
+using UnityEngine;
 
 namespace BlasII.ModdingAPI;
+
+[HarmonyPatch(typeof(AttackReceiverComponent), nameof(AttackReceiverComponent.OnAttackReceive), typeof(AttackHit), typeof(Collision2D))]
+class t
+{
+    public static void Postfix(AttackReceiverComponent __instance, AttackHit hit)
+    {
+        if (__instance.IsInvincible() || __instance.IsDodging)
+            return;
+
+        var data = hit.hitData.attackDamage;
+
+        if (_lastReceiver == __instance && _lastAttack == data)
+        {
+            ModLog.Error($"x{++_count}");
+            return;
+        }
+
+        ModLog.Error($"{__instance.name} received a hit: {hit?.attackID?.name} ({hit?.attackID?.GetType().Name})");
+
+        ModLog.Warn("Physical sources");
+        for (int i = 0; i < data.physicalAttackSources.Length; i++)
+        {
+            var attack = data.physicalAttackSources[i];
+            ModLog.Info($"{attack?.attackType?.name}: {attack.baseDamage} damage");
+        }
+
+        ModLog.Warn("Elemental sources");
+        for (int i = 0; i < data.elementalAttackSources.Length; i++)
+        {
+            var attack = data.elementalAttackSources[i];
+            ModLog.Info($"{attack?.attackType?.name}: {attack.baseDamage} damage");
+        }
+
+        ModLog.Warn("Status effect sources");
+        for (int i = 0; i < data.statusAttackSources.Length; i++)
+        {
+            var attack = data.statusAttackSources[i];
+            ModLog.Info($"{attack?.attackType?.name}: {attack.baseDamage} damage");
+        }
+
+        _lastReceiver = __instance;
+        _lastAttack = data;
+        _count = 1;
+    }
+
+    private static AttackReceiverComponent _lastReceiver;
+    private static AttackSourceData _lastAttack;
+    private static int _count = 0;
+}
 
 [HarmonyPatch(typeof(Core), nameof(Core.Awake))]
 class Core_Awake_Patch
